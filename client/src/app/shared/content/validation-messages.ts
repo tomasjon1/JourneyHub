9be7f984dd1daf0436cf014be4f74bc93486c13b
tmneth <80415416+tmneth@users.noreply.github.com>
2023-11-28 12:34:14 +0200

    Add validation messages

---
+++ client/src/app/shared/content/validation-messages.ts
@@ -0,0 +1,35 @@
+export const validationMessages = {
+  auth: {
+    login: {
+      email: {
+        required: 'Email is required.',
+        email: 'Please enter a valid email.',
+      },
+      password: {
+        required: 'Password is required',
+      },
+    },
+    register: {
+      username: {
+        required: 'Username is required',
+        minlength: 'Username must be at least 4 characters long.',
+        maxlength: 'Username cannot be more than 16 characters long.',
+        pattern: 'Username must consist of letters and numbers only.',
+      },
+      email: {
+        required: 'Email is required',
+        email: 'Please enter a valid email.',
+      },
+      password: {
+        required: 'Password is required',
+        minlength: 'Password must be at least 8 characters long.',
+        pattern:
+          'Password must include uppercase and lowercase letters, a number, and a special character.',
+      },
+      confirmPassword: {
+        required: 'Please repeat entered password.',
+        mustMatch: 'Passwords must match.',
+      },
+    },
+  },
+};
