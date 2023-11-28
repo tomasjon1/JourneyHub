0d4e0e0506ede465094adb8386b7a8760271ee55
tmneth <80415416+tmneth@users.noreply.github.com>
2023-11-28 11:00:51 +0200

    Add ngx-toastr

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,11 +4,17 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app/app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(APP_ROUTES),
     provideAnimations(),
+    provideToastr({
+      timeOut: 2500,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
   ],
 });
