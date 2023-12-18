6cff7c0bee801e0fb27793c586791dcceebb0b88
tmneth <80415416+tmneth@users.noreply.github.com>
2023-12-18 19:38:59 +0200

    Remove unused code

--- Api/JourneyHub.Common/MapperProfiles/TripProfiles.cs
+++ Api/JourneyHub.Common/MapperProfiles/TripProfiles.cs
@@ -14,9 +14,7 @@ namespace JourneyHub.Common.MapperProfiles
                 .ForMember(dest => dest.RouteDescription, opt => opt.MapFrom(src => src.RouteDescription))
                 .ForMember(dest => dest.Visibility, opt => opt.MapFrom(src => src.Visibility))
                 .ForMember(dest => dest.Distance, opt => opt.MapFrom(src => src.Distance))
-                .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => src.Duration))
-                .ForMember(dest => dest.Distance, opt => opt.MapFrom(src => JsonConvert.SerializeObject(src.Distance)))
-                .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => JsonConvert.SerializeObject(src.Duration)));
+                .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => src.Duration));
         }
     }
 }
