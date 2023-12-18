b16218f54ae04a98cb3681f2f83f87bb5d5a108d
alojine <benaspaliokas@gmail.com>
2023-12-18 23:44:34 +0200

    profile html

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ProfileService } from './profile.service';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -6,4 +9,38 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   imports: [],
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+
+  userProfileForm: FormGroup;
+
+  defaultState = {
+    username: 'username',
+    email: 'email',
+    password: 'password',
+    newPassword: 'new password',
+    confirmNewPassword: 'confirm password',
+  };
+
+  private _formBuilder = inject(FormBuilder);
+  private _profileService = inject(ProfileService);
+  private _router = inject(Router)
+
+  constructor() {
+    this._profileService
+    this.userProfileForm = this._formBuilder.group(
+      {
+        username: [
+          this.defaultState.username,
+        ],
+        email: [
+          this.defaultState.email,
+        ],
+      }
+    )
+  }
+
+  onSubmit() {
+
+  }
+
+}
