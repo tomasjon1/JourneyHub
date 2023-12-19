e8889e326cc579687aa779b9df176931d995347e
tmneth <80415416+tmneth@users.noreply.github.com>
2023-12-19 20:18:40 +0200

    Allow swagger

--- Api/JourneyHub.Api/Program.cs
+++ Api/JourneyHub.Api/Program.cs
@@ -63,11 +63,11 @@ namespace JourneyHub
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            // if (app.Environment.IsDevelopment())
+            // {
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            // }
 
             app.UseExceptionHandler(c => c.Run(ExceptionHandlerMiddleware.InvokeAsync));
 
