f0831da46be810390b9af9196b1c2bc90ccc1c69
tmneth <80415416+tmneth@users.noreply.github.com>
2023-12-19 11:38:09 +0200

    Add profile page

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,9 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ProfileService } from './profile.service';
+import {
+  FormGroup,
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { InputComponent } from '../input/input.component';
+import { ToastrService } from 'ngx-toastr';
+import { validationMessages } from '../shared/content/validation-messages';
 
 @Component({
   standalone: true,
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, InputComponent],
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit {
+  profileForm: FormGroup;
+  loginValidationMessages = validationMessages.auth.login;
+
+  constructor() {
+    this.profileForm = this._formBuilder.group({
+      email: [this.defaultState.email, [Validators.required, Validators.email]],
+      name: [this.defaultState.name, [Validators.required]],
+    });
+  }
+
+  _profileService = inject(ProfileService);
+  private _toastrService = inject(ToastrService);
+  private _formBuilder = inject(NonNullableFormBuilder);
+
+  defaultState = {
+    email: '',
+    name: '',
+  };
+
+  userDetails: any;
+
+  ngOnInit(): void {
+    this._profileService.getUserProfile().subscribe({
+      next: (response: any) => {
+        this.userDetails = response.data;
+        this.profileForm.patchValue({
+          email: this.userDetails.email,
+          name: this.userDetails.userName, // if you have the password
+        });
+      },
+      error: (error: any) => {},
+    });
+  }
+
+  onSubmit() {
+    if (this.profileForm.invalid) {
+      Object.keys(this.profileForm.controls).forEach((field) => {
+        const control = this.profileForm.get(field);
+        control?.markAsTouched({ onlySelf: true });
+      });
+      return;
+    }
+
+    // this._authService.login(this.signInForm.value).subscribe({
+    //   next: (response: any) => {
+    //     this._router.navigate(['/explore']);
+    //   },
+    //   error: (error: any) => {
+    //     this._toastrService.error('', error.error.Error.Message);
+    //   },
+    // });
+  }
+}
---
+++ client/src/app/profile/profile.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  apiUrl: string = 'https://localhost:5001';
+
+  private readonly _http = inject(HttpClient);
+
+  public getUserProfile(): any {
+    const userData: {
+      name: string;
+      email: string;
+      _token: string;
+      _tokenExpirationDate: Date;
+    } = JSON.parse(localStorage.getItem('userData') || '{}');
+
+    if (!userData) {
+      return;
+    }
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userData!._token}`,
+      }),
+    };
+
+    return this._http.get(`${this.apiUrl}/api/Users`, httpOptions);
+  }
+}
